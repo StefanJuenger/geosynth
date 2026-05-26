7576f07c6cfe92d2b239c27320ce2c3a2a8d740d
Jünger <stefan.juenger@gesis.org>
2026-05-26 11:00:04 +0200

    nicer internal data workflow

--- R/datasets.R
+++ R/datasets.R
@@ -1,9 +1,95 @@
-#' municipalities_inhabitants.R
+#' Census 2011 Inhabitants Data
 #'
-#' @source <https://opendata-esridech.hub.arcgis.com/>
-# "municipalities_inhabitants"
+#' @format A data frame/simple feature collection with 361,478 rows and 13 columns:
+#' \describe{
+#'   \item{inspid1km}{INSPIRE 1 sqkm identifier}
+#'   \item{inhabitants}{Inhabitants count}
+#'   \item{geometry}{Point geometry column (\code{EPSG:3035})}
+#'   \item{ags_YEAR}{German municipality identifier for the corresponding year ("Allgemeiner Gemeindeschlüssel")}
+#' }
+#'
+#' @source https://www.zensus2011.de
+"census_inhabitants"
 
-#' census_inhabitants.R
+#' Municipality Level Data
 #'
-#' @source <https://www.zensus2011.de>
-# "census_inhabitants"
+#' @format A data frame with ~11,000 rows (depending on the year) and 6 columns:
+#' \describe{
+#'   \item{lan}{2-digit German federal state identifier}
+#'   \item{ags}{8-digit German municipality identifier ("Allgemeiner Gemeindeschlüssel")}
+#'   \item{gkpol}{1-digit political municipality size class identifier ("Politische Gemeindegrößenklasse"), see \url{https://www.bbsr.bund.de/BBSR/DE/forschung/raumbeobachtung/Raumabgrenzungen/deutschland/gemeinden/StadtGemeindetyp/StadtGemeindetyp.html}}
+#'   \item{regiostar7}{2-digit summarized regional spatial typology statistic identifier ("Regionalstatistische Raumtypologie, Zusammengefasster regionalstatischer Raumtyp"), see \url{https://www.bmv.de/SharedDocs/DE/Artikel/G/regionalstatistische-raumtypologie.html}}
+#'   \item{regiostar17}{3- digit regional spatial statistic type identifier "Regionalstatistische Raumtypologie, Regionalstatischer Raumtyp", \url{see https://www.bmv.de/SharedDocs/DE/Artikel/G/regionalstatistische-raumtypologie.html}}
+#'   \item{inhabitants}{Inhabitants count in absolute numbers}
+#' }
+#'
+#' @name mun_YEAR
+#'
+#' @source https://opendata-esridech.hub.arcgis.com
+#' @source https://www.destatis.de/DE/Themen/Laender-Regionen/Regionales/Gemeindeverzeichnis/Administrativ/15-regiostar.html
+#' @source https://www.bbsr.bund.de/BBSR/DE/forschung/raumbeobachtung/Raumabgrenzungen/deutschland/gemeinden/StadtGemeindetyp/StadtGemeindetyp.html
+"mun_2012"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2013"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2014"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2015"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2016"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2017"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2018"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2019"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2020"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2021"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2022"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2023"
+
+#' @rdname mun_YEAR
+#' @format NULL
+"mun_2024"
+
+#' Fake Survey Coordinates
+#'
+#' Fake survey coordinates that are not even synthetic and only serve
+#' demonstration purposes
+#'
+#' @format A data frame/simple feature collection with 3,004 rows and 3 columns:
+#' \describe{
+#'   \item{id}{Fake respondent identifier}
+#'   \item{ags}{8-digit German municipality identifier ("Allgemeiner Gemeindeschlüssel")}
+#'   \item{geometry}{Point geometry column (\code{EPSG:3035})}
+#' }
+#'
+#' @source Created using \code{geosynth:::create_fake_survey_coordinates()}
+"fake_survey_coordinates"
+
